4e50d90851e2039356b4a09fe503399f2512bf37
Chowo <bob-xiii@rambler.ru>
2024-09-18 22:15:47 +0200

    Finished Homework 3.5

---
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -0,0 +1,71 @@
+package ru.hogwarts.school.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.exception.AvatarNotFoundException;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+@Service
+@Transactional
+public class AvatarServiceImpl implements AvatarService {
+    private final AvatarRepository avatarRepository;
+    private final StudentService studentService;
+
+    @Value(value = "${path.to.avatars.folder}")
+    private String avatarsDir;
+
+
+    public AvatarServiceImpl(AvatarRepository avatarRepository, StudentService studentService) {
+        this.avatarRepository = avatarRepository;
+        this.studentService = studentService;
+    }
+
+    @Override
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        Student student = studentService.getStudentById(studentId);
+        Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream inputStream = avatarFile.getInputStream();
+                OutputStream outputStream = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 1024);
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 1024)
+        ) {
+            bufferedInputStream.transferTo(bufferedOutputStream);
+        }
+        Avatar avatar = findAvatarByStudentId(studentId);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatarRepository.save(avatar);
+    }
+
+    @Override
+    public Avatar findAvatarByStudentId(Long studentId) {
+        return avatarRepository.findAvatarByStudentId(studentId).orElse(new Avatar());
+    }
+
+    @Override
+    public Avatar findAvatar(Long id) {
+        return avatarRepository.findById(id).orElseThrow(() -> new AvatarNotFoundException(id));
+    }
+
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+
+}
